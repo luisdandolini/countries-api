0a4465a452c4e8ecb91dd0fbd6ec0848ab573ce4
Luis Dandolini <luisfernandodandoliniduarte@gmail.com>
2022-10-31 16:39:39 -0300

    add create service country

--- src/app/services/country.service.ts
+++ src/app/services/country.service.ts
@@ -1,10 +1,27 @@
+import { Country } from './../models/country';
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountryService {
 
-  constructor() { }
+  url = "https://restcountries.com/v3.1/all";
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+  constructor(
+    private httpCliente: HttpClient
+  ) { }
+
+  public getCountries(): Observable<Country[]> {
+    return this.httpCliente.get<Country[]>(this.url);
+  }
 
 } 
