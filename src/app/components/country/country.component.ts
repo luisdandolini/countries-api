7c60b65c97e8781d8b9119e82a4719700377566a
Luis Dandolini <luisfernandodandoliniduarte@gmail.com>
2022-10-31 16:40:06 -0300

    add create component country

--- src/app/components/country/country.component.ts
+++ src/app/components/country/country.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from 'src/app/models/country';
+import { CountryService } from 'src/app/services/country.service';
 
 @Component({
   selector: 'app-country',
@@ -7,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CountryComponent implements OnInit {
 
+  countryPreview: Country[] = [];
 
-  constructor() { }
+  constructor(
+    public country: CountryService
+  ) { }
 
   ngOnInit(): void {
+    this.getCountries();
+  }
+
+  getCountries() {
+    this.country.getCountries().subscribe((data: any) => {
+      this.countryPreview = data;
+      console.log(this.countryPreview);
+    })
   }
 
 }
